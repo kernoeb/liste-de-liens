2a7d66565a3b6537f4bc4ec1de82193d9cfd631a
kernoeb <kernoeb@protonmail.com>
2023-04-19 16:17:55 +0200

    add pwa mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 import zipPack from 'vite-plugin-zip-pack'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // Utilities
 import { defineConfig } from 'vite'
 
 const IS_SINGLE_FILE = process.env.SINGLE_FILE === 'true'
-console.log('IS_SINGLE_FILE', IS_SINGLE_FILE)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +23,29 @@ export default defineConfig({
     }),
     IS_SINGLE_FILE ? viteSingleFile() : undefined,
     IS_SINGLE_FILE ? zipPack({ outDir: '.' }) : undefined,
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'maskable_icon.png'],
+      manifest: {
+        name: 'Liste de liens',
+        short_name: 'Liste de liens',
+        description: 'Le site de liens utiles ⭐ français 🇫🇷 100% privacy friendly 🕵',
+        theme_color: '#BB86FC',
+        lang: 'fr',
+        icons: [
+          {
+            src: 'maskable_icon_x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable_icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ].filter(v => v !== undefined),
   define: { 'process.env': {} },
   resolve: {
